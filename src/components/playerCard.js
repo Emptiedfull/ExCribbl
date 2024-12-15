cee4abfc9c226927ad84e734d75ddf0c6e7f5bc0
empty <atharavagarwal11@gmail.com>
2024-12-15 20:51:30 +0530

    major ui rehaul(animations n font)

--- src/components/playerCard.js
+++ src/components/playerCard.js
@@ -1,19 +1,27 @@
 import styles from '../styles/playercard.module.css';
+import {motion} from "framer-motion"
+import {useState} from "react";
+
 
 export default function PlayerCard({name,score,active,self}) {
+    const [hovering,setHovering] = useState(false)
+
+
+    
+
     return (
-        <div className={(styles.playerCard)} style={{
+        <motion.div onHoverStart={()=>setHovering(true)} onHoverEnd={()=>setHovering(false)} whileHover={{scale:1.1}} transition={{type:"spring",stiffness:400}} className={(styles.playerCard)} style={{
             backgroundColor: self ? "#BCCCDC" : "white"
         }}>
             <div className={styles.playerName}>
                 <span>{active ? "🖌️" : ""}</span>
-                <span>{name}</span>
+                <span>{name}{self ? "(you)":""}</span>
 
             </div>
-            <div className={styles.playerScore}>
+            <div className={styles.playerScore} style={{color: !hovering ? "black" : "green",fontWeight: !hovering ? "normal" : "bold"}}>
                 {score}
             </div>
-        </div>
+        </motion.div>
     )
 
 }
